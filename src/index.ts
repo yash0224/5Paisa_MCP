cd05c7a1c58626d0fefb315f07d77a23aa4b37cc
yash0224 <f20200588@hyderabad.bits-pilani.ac.in>
2025-06-28 13:11:33 +0530

    fixed docker node version dependenies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 #!/usr/bin/env node
 
 import { MCPServer } from "mcp-framework";
+
 const server = new MCPServer();
+
+// Keep the process alive
+process.on('SIGINT', () => {
+  console.log('Shutting down gracefully...');
+  process.exit(0);
+});
+
+process.on('SIGTERM', () => {
+  console.log('Shutting down gracefully...');
+  process.exit(0);
+});
+
+// Prevent the process from exiting
+process.stdin.resume();
+
 server.start();
